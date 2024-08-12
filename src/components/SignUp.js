b264a127ec3d55a49d65d4580d15d9287d0ddd7d
Shubham <sk715152@gmail.com>
2024-08-12 14:03:01 +0530

    SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -50,7 +50,7 @@ const SignUp = () => {
         </div>
         <div className="absolute bottom-0 h-3 w-full bg-[rgb(35,35,35)]"></div>
       </div>
-      <div className="h-[60vh] bg-black text-white">
+      <div className="flex h-[65vh] bg-black text-white relative">
         <div className=" flex items-center w-[60%] mx-auto">
           <div className="max-w-[50%] flex flex-col gap-4">
             <h1 className="text-5xl font-bold">Enjoy on your TV</h1>
@@ -66,19 +66,106 @@ const SignUp = () => {
               className="block w-auto relative z-20"
             />
             <div>
-            <video
-              className="absolute top-[50%] left-[50%] transform -translate-x-[51%] -translate-y-[54%]"
-              width="416"
-              autoPlay muted loop
-            >
-              <source
-                src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v"
-                type="video/mp4"
-              />
-            </video>
+              <video
+                className="absolute top-[50%] left-[50%] transform -translate-x-[51%] -translate-y-[54%]"
+                width="416"
+                autoPlay
+                muted
+                loop
+              >
+                <source
+                  src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v"
+                  type="video/mp4"
+                />
+              </video>
             </div>
           </div>
         </div>
+        <div className="absolute bottom-0 h-3 w-full bg-[rgb(35,35,35)]"></div>
+      </div>
+      <div className="flex h-[65vh] bg-black text-white relative">
+        <div className="flex items-center w-[60%] mx-auto">
+          <div className="relative">
+            <img
+              alt=""
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg"
+            />
+            <div className=" flex items-center rounded-lg border p-2 gap-6 w-[70%] absolute bottom-[8%] left-[15%] bg-black">
+              <div>
+                <img
+                  className="h-20"
+                  alt=""
+                  src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png"
+                />
+              </div>
+              <div>
+                <div className="text-white">Stranger Things</div>
+                <div className="text-blue-800">Downloading...</div>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-[50%] flex flex-col gap-4">
+            <h1 className="text-5xl font-bold">
+              Download your shows to watch offline
+            </h1>
+            <p className="text-[1.5rem] font-normal">
+              Save your favourites easily and always have something to watch.
+            </p>
+          </div>
+        </div>
+        <div className="absolute bottom-0 h-3 w-full bg-[rgb(35,35,35)]"></div>
+      </div>
+
+      <div className="flex h-[65vh] bg-black text-white relative">
+        <div className=" flex items-center w-[60%] mx-auto">
+          <div className="max-w-[50%] flex flex-col gap-4">
+            <h1 className="text-5xl font-bold">Watch everywhere</h1>
+            <p className="text-[1.5rem] font-normal">
+              Stream unlimited movies and TV shows on your phone, tablet,
+              laptop, and TV.
+            </p>
+          </div>
+          <div className="relative">
+            <img
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png"
+              alt=""
+              className="block w-auto relative z-10"
+            />
+            <div>
+              <video
+                className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[85%] w-[65%]"
+                autoPlay
+                muted
+                loop
+              >
+                <source
+                  src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices-in.m4v"
+                  type="video/mp4"
+                />
+              </video>
+            </div>
+          </div>
+        </div>
+        <div className="absolute bottom-0 h-3 w-full bg-[rgb(35,35,35)]"></div>
+      </div>
+      <div className="bg-black h-[65vh] flex items-center text-white relative">
+        <div className="w-[65%] mx-auto flex items-center">
+          <div>
+            <img
+              src="https://occ-0-2085-2164.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVr8nYuAg0xDpXDv0VI9HUoH7r2aGp4TKRCsKNQrMwxzTtr-NlwOHeS8bCI2oeZddmu3nMYr3j9MjYhHyjBASb1FaOGYZNYvPBCL.png?r=54d"
+              alt=""
+            />
+          </div>
+          <div className="max-w-[50%]">
+            <h1 className="text-5xl font-bold">Create profiles for kids</h1>
+            <p className="text-[1.5rem] font-normal">
+              Send children on adventures with their favourite characters in a
+              space made just for them—free with your membership.
+            </p>
+          </div>
+        </div>
+
+        <div className="absolute bottom-0 h-3 w-full bg-[rgb(35,35,35)]"></div>
       </div>
     </div>
   );
